0c3f855c83c871d9df55a27e53f93bbfcec80952
mb839 <matthewbrown839@gmail.com>
2016-03-24 13:07:46 +0000

    Bug-fixes - now working.
    Commented.
    Changed for (int i = 9; i >= 0; i--) to for (int i = 8...
    Removed reference to "input2" variable as this is a hangover from the R algorithm and actually does nothing.
    Removed the resetting of sum.
    Made the std::pow more explicit.
    Tested with Rcpp and doesn't seem to fall over, produces identical output for all test input to the R algorithm and achieves speedup of ~36x (1 million runs, Rcpp (average of 4) = 13.01 seconds, native R = 472.37).

--- studyIDCheckSumBoolRCPP.cpp
+++ studyIDCheckSumBoolRCPP.cpp
@@ -17,33 +17,39 @@ bool studyCheckSum(std::string studyID) {
   
   // sets up the base as 37, this can be changed if needed
   int base = 37;
-  
-  // std::string input2 = studyID.substr(0, 9);
-  
+
   // sets up the coidePoint vector
   std::vector<int> codePoint(9, 0);
   // sets up the code point list as a string whose characters correspond to
   // code point + 1
   std::string cypher = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ*";
   
+  // puts the elements of the cypher corresponding to elements in studyID
+  // into the codePoint vector
   for (int i = 0; i < 9; i++) {
     std::string j = studyID.substr(i, 1);
     codePoint[i] = cypher.find_first_of(j);
   }
   
+  // initialises sum as 0
   int sum = 0;
   
-  for (int i = 9; i >= 0; i--) {
-    codePoint[i] = codePoint[i] * std::pow(2, std::abs(i-9));
+  // working backwards from the final element, multiplies the code point 
+  // by 2^n where n is the nth cycle in the iteration then adds to sum
+  for (int i = 8; i >= 0; i--) {
+		codePoint[i] = codePoint[i] * (int)std::pow( static_cast<double>(2), 
+									  std::abs(i-9));
     sum += codePoint[i];
   }
   
+  // takes the remainder of sum mod base (37 by default)
   int pointCode = (((base + 1) - (sum % base)) % base);
-  
-  sum = 0;
-  
+
+  // determines the correct check digit
   std::string checkDigit = cypher.substr(pointCode, 1);
   
+  // returns a bool corresponding to computed == actual check digit
+  // the last 2 steps can be combined but actually slow things down
   return (checkDigit == studyID.substr(9, 1));
   
 }
